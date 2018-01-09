6c14826fcda2f9034e19b49676b4fccfefce0cdd
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-01-08 23:40:51 -0600

    partition understood

--- kotlin-koans/src/ii_collections/n21Partition.kt
+++ kotlin-koans/src/ii_collections/n21Partition.kt
@@ -12,5 +12,6 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    val (customersWithMoreDelievered, customersWithMoreUndeliveredOrders) = this.customers.partition { it.orders.all { it.isDelivered } }
+    return customersWithMoreUndeliveredOrders.toSet()
 }
