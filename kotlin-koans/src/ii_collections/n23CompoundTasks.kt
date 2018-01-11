9ff8bc8e1e124c4350b9bcaf62ef4d8657737fda
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-01-10 22:24:38 -0600

    folding is learned. wip

--- kotlin-koans/src/ii_collections/n23CompoundTasks.kt
+++ kotlin-koans/src/ii_collections/n23CompoundTasks.kt
@@ -2,7 +2,7 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+   return this.customers.filter { it.orderedProducts.contains(product) }
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
