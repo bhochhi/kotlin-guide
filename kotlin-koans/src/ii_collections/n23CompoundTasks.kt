b666427af5e9c1b3c8a28755f38eef0b4af37e78
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-01-14 22:55:20 -0600

    collections

--- kotlin-koans/src/ii_collections/n23CompoundTasks.kt
+++ kotlin-koans/src/ii_collections/n23CompoundTasks.kt
@@ -8,11 +8,11 @@ fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return this.orders.filter { it.isDelivered }.flatMap { it.products }.maxBy { it.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return this.customers.flatMap { it.orders }.flatMap { it.products }.count { it ==product }
 }
