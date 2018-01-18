5afacbcab3b45deb1fa9b59ece1620f756810996
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-01-17 23:03:15 -0600

    implementing comparable

--- kotlin-koans/src/iii_conventions/MyDate.kt
+++ kotlin-koans/src/iii_conventions/MyDate.kt
@@ -1,6 +1,13 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate>{
+    override fun compareTo(other: MyDate) = when {
+        year!=other.year ->year - other.year
+        month!=other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
