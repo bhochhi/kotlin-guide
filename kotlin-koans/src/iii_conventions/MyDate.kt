b3cc52e6f77f52217dc15ee6803091313aee9f81
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-01-19 22:58:21 -0600

    RangeTo Implemented

--- kotlin-koans/src/iii_conventions/MyDate.kt
+++ kotlin-koans/src/iii_conventions/MyDate.kt
@@ -9,7 +9,11 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+
+   return DateRange(this,other)
+
+}
 
 enum class TimeInterval {
     DAY,
@@ -18,7 +22,6 @@ enum class TimeInterval {
 }
 
 class DateRange(override  val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>{
-
 
     override fun contains(value: MyDate): Boolean {
         return start<=value && value<=endInclusive
